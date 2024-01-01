8949fe8917b0e6659dc8cb4f13ad740204980ef0
brahamabull <cenazoran@yahoo.com>
2024-01-01 22:44:17 +0100

    initial code base commit

---
+++ src/main/java/com/coding/challenge/JetCodingChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.coding.challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JetCodingChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JetCodingChallengeApplication.class, args);
+	}
+
+}
